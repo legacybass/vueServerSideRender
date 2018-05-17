65cabdfced3a7306ec52c95daa3016e571e53dba
Daniel Beus <legacybass.developer@outlook.com>
2018-05-17 15:16:21 -0600

    Server-side render first success

--- ClientApp/boot-server.js
+++ ClientApp/boot-server.js
@@ -1,23 +1,27 @@
-var prerendering = require('aspnet-prerendering');
-const renderer = require('vue-server-renderer').createRenderer();
-const app = require('./app').default;
+import { createServerRenderer } from 'aspnet-prerendering';
+import { createRenderer } from 'vue-server-renderer';
+//const renderer = require('vue-server-renderer').createRenderer();
+//const app = require('./app').default;
 
-module.exports = prerendering.createServerRenderer(function (params) {
-    return new Promise((resolve, reject) => {
-        const basename = params.baseUrl.substring(0, params.baseUrl.length - 1);
-        const urlAfterBasename = params.url.substring(basename.length);
-        const store = configureStore(null, params.data);
+export default createServerRenderer(function (params) {
+	return new Promise((resolve, reject) => {
 
-        //store.dispatch(urlAfterBasename); // route to the incoming url
+		return resolve({ html: '<h1>You made it!</h1>' });
+
+		//const basename = params.baseUrl.substring(0, params.baseUrl.length - 1);
+		//const urlAfterBasename = params.url.substring(basename.length);
+		//const store = configureStore(null, params.data);
+
+		////store.dispatch(urlAfterBasename); // route to the incoming url
 
 
-        const html = renderer.renderToString(app)
+		//const html = renderer.renderToString(app)
 
-        params.domainTasks.then(() => {
-            resolve({
-                html,
-                globals: { initialVuexState: state.getState() }
-            })
-        });
-    });
+		//params.domainTasks.then(() => {
+		//	resolve({
+		//		html,
+		//		globals: { initialVuexState: state.getState() }
+		//	})
+		//});
+	});
 });
